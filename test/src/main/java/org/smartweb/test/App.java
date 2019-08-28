60f36625113616246d7275ff7b40765eedbbfa48
user <user@user-PC>
2019-08-28 15:11:33 +0900

    Product

---
+++ test/src/main/java/org/smartweb/test/App.java
@@ -0,0 +1,13 @@
+package org.smartweb.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ test/src/main/java/org/smartweb/test/Product.java
@@ -0,0 +1,8 @@
+package org.smartweb.test;
+
+public class Product {
+	public static void main( String[] args )
+    {
+        System.out.println( "Product 생성자 Call" );
+    }
+}
